cc3aa209c96b77b7d95faf269cbc7e093bf2d835
Mostafa Wahied <97263286+Mostafa-Wahied@users.noreply.github.com>
2022-09-23 22:06:40 -0700

    http to https, air script

--- air/script.js
+++ air/script.js
@@ -1,10 +1,10 @@
 // code for map
-var OSM_URL = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-var OSM_ATTRIB = '&copy;  <a  href="http://openstreetmap.org/copyright">OpenStreetMap</a>  contributors';
+var OSM_URL = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+var OSM_ATTRIB = '&copy;  <a  href="https://openstreetmap.org/copyright">OpenStreetMap</a>  contributors';
 var osmLayer = L.tileLayer(OSM_URL, { attribution: OSM_ATTRIB });
 
 var WAQI_URL = "https://tiles.waqi.info/tiles/usepa-aqi/{z}/{x}/{y}.png?token=_TOKEN_ID_";
-var WAQI_ATTR = 'Air  Quality  Tiles  &copy;  <a  href="http://waqi.info">waqi.info</a>';
+var WAQI_ATTR = 'Air  Quality  Tiles  &copy;  <a  href="https://waqi.info">waqi.info</a>';
 var waqiLayer = L.tileLayer(WAQI_URL, { attribution: WAQI_ATTR });
 
 var map = L.map('map').setView([51.505, -0.09], 11);
